2576d1a47fe2ebfee3bd8d5122f294b1430adb35
pachi <1772343187@qq.com>
2022-04-30 15:55:56 +0800

    使用反射机制改进代码

--- src/utils/SortingHelper.java
+++ src/utils/SortingHelper.java
@@ -3,6 +3,9 @@ package utils;
 import sorting.InsertionSort;
 import sorting.SelectionSort;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * @author pachi
  * @date 2021-6-13
@@ -24,40 +27,35 @@ public class SortingHelper {
 
     public static <E extends Comparable<E>> void sortTest(String sortName, E[] arr) {
 
-        long startTime = System.nanoTime();
+        try {
 
-        if (sortName.equals("SelectionSort")) {
-            SelectionSort.sort(arr);
-        } else if (sortName.equals("InsertionSort")) {
-            InsertionSort.sort(arr);
-            InsertionSort.sort3(arr);
+            Class sortClass = Class.forName("sorting." + sortName);
+            Method sortMethod = sortClass.getMethod("sort", Comparable[].class);
+            Object[] params = new Object[]{arr};
+
+            long startTime = System.nanoTime();
+
+            sortMethod.invoke(null, params);
+
+            long endTime = System.nanoTime();
+
+            double time = (endTime - startTime) / 1000000000.0;
+
+            if (!SortingHelper.isSorted(arr)) {
+                throw new RuntimeException(sortName + " failed");
+            }
+
+            System.out.println(String.format("%s , n = %d : %f s", sortName, arr.length, time));
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
         }
-//        } else if (sortName.equals("MergeSort")) {
-//            MergeSort.sort(arr);
-//        } else if (sortName.equals("MergeSortBU")) {
-//            MergeSort2.sort(arr);
-//        } else if (sortName.equals("QuickSort")) {
-//            QuickSort.sort(arr);
-//        } else if (sortName.equals("QuickSort2Ways")) {
-//            QuickSort2Ways.sort(arr);
-//        } else if (sortName.equals("QuickSort3Ways")) {
-//            QuickSort3Ways.sort(arr);
-//        } else if (sortName.equals("HeapSort")) {
-//            HeapSort.sort(arr);
-//        } else if (sortName.equals("HeapSortInPlace")) {
-//            HeapSort.sortInPlace(arr);
-//        } else if (sortName.equals("BubbleSort")) {
-//            BubbleSort.sort(arr);
-//        }
-
-        long endTime = System.nanoTime();
-
-        double time = (endTime - startTime) / 1000000000.0;
-
-        if (!SortingHelper.isSorted(arr)) {
-            throw new RuntimeException(sortName + " failed");
-        }
-
-        System.out.println(String.format("%s , n = %d : %f s", sortName, arr.length, time));
     }
 }
