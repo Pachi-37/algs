325ab8c382ee0bd0cf729fabe76d0cdce9946629
pachi <1772343187@qq.com>
2022-05-02 16:44:16 +0800

    使用动态数组实现 Stack

---
+++ src/structure/stack/ArrayStack.java
@@ -0,0 +1,62 @@
+package structure.stack;
+
+import structure.Array;
+
+public class ArrayStack<E> implements Stack<E> {
+
+    private Array<E> array;
+
+    public ArrayStack(int capacity) {
+        array = new Array<>(capacity);
+    }
+
+    public ArrayStack() {
+        array = new Array<>();
+    }
+
+    /**
+     * 这里调用 addLast() 方法，为了方便使用动态数组扩容功能
+     *
+     * @param e
+     */
+    @Override
+    public void push(E e) {
+        array.addLast(e);
+    }
+
+    @Override
+    public E pop() {
+        return array.removeLast();
+    }
+
+    @Override
+    public E peek() {
+        return array.getLast();
+    }
+
+    @Override
+    public int getSize() {
+        return array.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return array.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder res = new StringBuilder();
+        res.append("Stack: ");
+        res.append('[');
+
+        for (int i = 0; i < array.getSize(); i++) {
+            res.append(array.get(i));
+            if (i != array.getSize() - 1)
+                res.append(", ");
+        }
+        res.append("] top");
+        return res.toString();
+    }
+}
