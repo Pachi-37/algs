1c30121b986323e8a97d5f190921917ef55cca74
pachi <1772343187@qq.com>
2022-05-21 08:42:15 +0800

    生成退化快排取中间元素为标定点的特殊数组

--- src/utils/ArrayGenerator.java
+++ src/utils/ArrayGenerator.java
@@ -33,4 +33,37 @@ public class ArrayGenerator {
         return arr;
     }
 
+    /**
+     * 生成针对快速排序法标定点取中间值的特殊退化数组
+     *
+     * @param n 数据规模
+     * @return 针对快排的特定数组
+     */
+    public static Integer[] generateSpecialArray(int n) {
+
+        Integer[] ret = new Integer[n];
+        generateSpecialArray(ret, 0, 0, n - 1);
+
+        return ret;
+    }
+
+    private static void generateSpecialArray(Integer[] data, int val, int l, int r) {
+
+        if (l > r) {
+            return;
+        }
+
+        int mid = l + (r - l) / 2;
+        data[l] = val;
+        swap(data, l, mid);
+        val++;
+        generateSpecialArray(data, val, l, mid - 1);
+        generateSpecialArray(data, val, mid + 1, r);
+    }
+
+    private static void swap(Integer[] data, int l, int r) {
+        Integer temp = data[l];
+        data[l] = data[r];
+        data[r] = temp;
+    }
 }
