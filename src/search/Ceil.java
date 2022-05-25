4fef2438d98bf27162fd5a0bc3fcaa928d08216c
pachi <1772343187@qq.com>
2022-05-25 08:47:44 +0800

    lowerCeil()

--- src/search/Ceil.java
+++ src/search/Ceil.java
@@ -2,16 +2,44 @@ package search;
 
 public class Ceil {
 
-    private Ceil(){}
+    private Ceil() {
+    }
 
-    public <E extends Comparable<E>> int ceil(E[] data, E target){
+    public <E extends Comparable<E>> int ceil(E[] data, E target) {
 
-        int upper = Upper.search(data,target);
+        int upper = Upper.search(data, target);
 
-        if (upper - 1 >= 0 && data[upper - 1].compareTo(target) == 0){
+        if (upper - 1 >= 0 && data[upper - 1].compareTo(target) == 0) {
             return upper - 1;
         }
 
         return upper;
+    }
+
+    /**
+     * 寻找 >= target 的最小索引
+     *
+     * @param data
+     * @param target
+     * @param <E>
+     * @return
+     */
+    public <E extends Comparable<E>> int lowerCeil(E[] data, E target) {
+
+        int l = 0;
+        int r = data.length;
+
+        while (r > l) {
+
+            int mid = l + (r - l) / 2;
+
+            if (data[mid].compareTo(target) < 0){
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+
+        return l;
     }
 }
