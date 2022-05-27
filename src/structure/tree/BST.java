7f3eeaefaa8ca2aa96e45283d1b58a61c6f4c657
pachi <1772343187@qq.com>
2022-05-27 16:21:35 +0800

    二分搜索树

---
+++ src/structure/tree/BST.java
@@ -0,0 +1,63 @@
+package structure.tree;
+
+public class BST<E extends Comparable<E>> {
+
+    private class Node {
+
+        public E e;
+        public Node left, right;
+
+        public Node(E e) {
+            this.e = e;
+            left = null;
+            right = null;
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public BST() {
+        root = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // 向二分搜索树中插入不重复的元素
+    public void add(E e) {
+
+        root = add(root, e);
+    }
+
+    /**
+     * 返回插入新界点之后二叉搜索树的根
+     *
+     * @param node
+     * @param e
+     * @return
+     */
+    private Node add(Node node, E e) {
+
+        if (node == null) {
+            size++;
+            return new Node(e);
+        }
+
+        if (node.e.compareTo(e) == 0) {
+            return node;
+        } else if (node.e.compareTo(e) > 0) {
+            node.left = add(node.left, e);
+        } else {
+            node.right = add(node.right, e);
+        }
+
+        return node;
+    }
+}
