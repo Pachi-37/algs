a7e83e1fd2e6f56a25fe68bb05b196e36414788b
pachi <1772343187@qq.com>
2022-05-29 10:14:29 +0800

    BST 遍历

--- src/structure/tree/BST.java
+++ src/structure/tree/BST.java
@@ -124,4 +124,51 @@ public class BST<E extends Comparable<E>> {
             return contains(node.left, e);
         }
     }
+
+    /**
+     * 前序遍历
+     */
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+
+        if (node == null) {
+            return;
+        }
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    private void inOrder(Node node) {
+
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left);
+        System.out.println(node.e);
+        inOrder(node.right);
+    }
+
+    public void postOrder() {
+        postOrder(root);
+    }
+
+    private void postOrder(Node node) {
+
+        if (node == null) {
+            return;
+        }
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.e);
+    }
 }
