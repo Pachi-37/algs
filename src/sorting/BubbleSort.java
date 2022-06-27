967d1a98ee45961d3fca469baed4177dbf9ee186
pachi <1772343187@qq.com>
2022-06-27 15:57:08 +0800

    冒泡排序

---
+++ src/sorting/BubbleSort.java
@@ -0,0 +1,26 @@
+package sorting;
+
+public class BubbleSort {
+
+    private BubbleSort() {
+    }
+
+    public static <E extends Comparable<E>> void sort(E[] data) {
+
+        for (int i = 0; i < data.length - 1; i++) {
+
+            for (int j = 0; j < data.length - 1 - i; j++) {
+
+                if (data[j].compareTo(data[j + 1]) > 0) {
+                    swap(data, j, j + 1);
+                }
+            }
+        }
+    }
+
+    private static <E> void swap(E[] data, int l, int r) {
+        E temp = data[l];
+        data[l] = data[r];
+        data[r] = temp;
+    }
+}
